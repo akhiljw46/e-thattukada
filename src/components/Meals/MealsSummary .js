a4081d4eb3a969fa0ec081e8541e6be6a54d48e6
Akhil Augustin <akhiljw46@gmail.com>
2024-01-03 23:02:29 +0530

    Edited meals summary

--- src/components/Meals/MealsSummary .js
+++ src/components/Meals/MealsSummary .js
@@ -4,14 +4,14 @@ import { Parallax } from 'react-scroll-parallax';
 const MealsSummary = () => {
   return (
     <Parallax speed={20} className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>Thani Naadan Food, Delivered To You</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose your favorite drink, palahaaram or meal from our broad menu and
+        enjoy a delicious meal at home.
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        of course by experienced chef maaman!
       </p>
     </Parallax>
   );
